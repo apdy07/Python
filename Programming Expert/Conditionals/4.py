cd9c929a7fbd0f6881c9452f7ce5856375abc2ac
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-06 17:52:43 +0530

    Create 4.py

---
+++ Programming Expert/Conditionals/4.py
@@ -0,0 +1,11 @@
+# Write your code here.
+
+# You'll have to use the following strings:
+# 1) "How old are you? "
+# 2) "You can't ride the roller coaster."
+# 3) "You can ride the roller coaster!"
+age = int(input("How old are you? "))
+if age>=13:
+    print("You can ride the roller coaster!")
+else:
+    print("You can't ride the roller coaster.")
