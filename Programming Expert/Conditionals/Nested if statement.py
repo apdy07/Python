a8ae3c5114045faad0c310235e1dea95307b62c1
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-06 17:51:49 +0530

    Create Nested if statement.py

---
+++ Programming Expert/Conditionals/Nested if statement.py
@@ -0,0 +1,9 @@
+number = float(input("Enter a number: "))
+if number>0 and number%2==0:
+    print("It's a positive even number")
+
+    number1 = int(input("Enter a number: "))
+    if number1<0:
+        print("This is Negative")
+    else:
+        print("This is positive")
