588861d8d7f0881924f3cf678a1fedf9be865133
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-06 17:53:05 +0530

    Create 5.py

---
+++ Programming Expert/Conditionals/5.py
@@ -0,0 +1,18 @@
+# Write your code here.
+
+# You'll have to use the following strings:
+# 1) "What's your favorite programming language? "
+# 2) For Python, "Nice choice!"
+# 3) For Golang, "You're a cool kid I see..."
+# 4) For JavaScript, "Okay Mr. web developer."
+# 5) For anything else, "I don't know that language."
+
+pl = input("What's your favorite programming language? ")
+if pl == "Python":
+    print("Nice choice!")
+elif pl == "Golang":
+    print("You're a cool kid I see...")
+elif pl == "JavaScript":
+    print("Okay Mr. web developer.")
+else:
+    print("I don't know that language.")
