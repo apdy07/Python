40547f286b0c8c530341aa87ebff585fa2943cf8
apdy07 <119981586+apdy07@users.noreply.github.com>
2023-01-07 20:13:40 +0530

    Create 9.py

---
+++ Programming Expert/Lists/9.py
@@ -0,0 +1,16 @@
+# Write your code here.
+
+# You'll have to use the following strings:
+# 1) "Enter a string: "
+# 2) "Enter a number: "
+n0 = input("Enter a string: ")
+n1 = input("Enter a string: ")
+n2 = input("Enter a string: ")
+n3 = input("Enter a string: ")
+n4 = input("Enter a string: ")
+
+lst = [n0,n1,n2,n3,n4]
+m0 = int(input("Enter a number: "))
+m1 = int(input("Enter a number: "))
+m2 = int(input("Enter a number: "))
+print(lst[m0] + lst[m1] + lst[m2])
